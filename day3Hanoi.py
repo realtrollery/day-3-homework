4beb1237eb21bcde842cf19709e89180cc652103
Doyoon Yi <93254979+realtrollery@users.noreply.github.com>
2022-03-26 09:17:59 +0900

    Add files via upload

---
+++ day3Hanoi.py
@@ -0,0 +1,10 @@
+def tower_of_hanoi(disks, source, auxiliary, target):  
+    if(disks == 1):  
+        print('Move disk 1 from rod {} to rod {}.'.format(source, target))  
+        return  
+    tower_of_hanoi(disks - 1, source, target, auxiliary)  
+    print('Move disk {} from rod {} to rod {}.'.format(disks, source, target))  
+    tower_of_hanoi(disks - 1, auxiliary, source, target)
+
+dAmt = 4
+tower_of_hanoi(dAmt, '1','2', '3')
